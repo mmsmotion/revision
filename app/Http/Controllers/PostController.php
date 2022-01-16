c9ff37c92d938a4229e154f0df8c9b316f3aa99d
mmsmotion <mmsmotion9514@gmail.com>
2022-01-16 14:35:39 +0630

    update

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -130,7 +130,7 @@ class PostController extends Controller
      */
     public function show(Post $post)
     {
-        return $post->tags;
+        return $post;
         return view('post.show',compact('post'));
     }
 
